23f86b50dcaf25d43cb82d6f1bf85d41eb0e5040
artaleks9 <oshmarai@redhat.com>
2018-06-08 07:02:36 +0000

    qa-test: PR  update

--- src/main/java/com/codenvy/example/spring/GreetingController.java
+++ src/main/java/com/codenvy/example/spring/GreetingController.java
@@ -2,7 +2,7 @@ package com.codenvy.example.spring;
 // check PR
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
-
+// Update PR
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
